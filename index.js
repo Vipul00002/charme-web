3e7e0d94cca9f92a7e0f03041504db4a51d5a6f6
vipul <Vipulkhanna55>
2024-08-27 22:26:50 +0530

    intital commit

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ const express = require("express");
 const app = express();
 const { PORT } = require("./config");
 var path = require("path");
-// const { frontEndDirectoryPath } = require("./charme-web/index.cjs");
+const { frontEndDirectoryPath } = require("./charme-web/index.cjs");
 
-// const buildPath = path.join(frontEndDirectoryPath, "dist");
+const buildPath = path.join(frontEndDirectoryPath, "dist");
 
-// app.use(express.static(buildPath));
+app.use(express.static(buildPath));
 
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
-
-// app.get("/*", (req, res) => {
-
-//  res.sendFile(path.join(buildPath, "index.html"));
+// app.get("/ping", (req, res) => {
+//   res.send("pong");
 // });
+
+app.get("/*", (req, res) => {
+ res.sendFile(path.join(buildPath, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("listening ----------------", PORT);
