f56475101480ac1340ab136a2b7ecba58b0a9cd9
Peeyush Varyani <reachoutpv@gmail.com>
2024-08-27 23:36:43 +0530

    update/videoTrim

--- charme-web/src/App.jsx
+++ charme-web/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import videoD from "./assets/desktop-new.mp4";
 import videoM from "./assets/mobile-new.mp4";
 import { useMediaQuery } from "react-responsive";
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <video autoPlay loop playsInline muted ref={videoRef} className="h-full">
-      <source src={isPortrait?videoM:videoD} type="video/mp4" />
+      <source src={isPortrait ? videoM : videoD} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
